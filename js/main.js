933ee23dd84b30d8419381d6e33767b161512d3c
Ivo Murrell <ivoisbelongtous@gmail.com>
2016-09-03 18:20:40 +0100

    Initial commit.
    The site is a dumb landing page!

---
+++ js/main.js
@@ -0,0 +1,33 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setClearColor( 0xffffff );
+document.body.appendChild( renderer.domElement );
+
+var fLoader = new THREE.FontLoader()
+var text;
+fLoader.load(
+	"fonts/helvetiker_regular.typeface.json",
+	function(font) {
+		var geometry = new THREE.TextGeometry( "Ivo", {size: 5, font: font, height: 2.5} );
+		var material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+		text = new THREE.Mesh( geometry, material );
+		scene.add( text );
+	}
+)
+
+camera.position.y = 2;
+camera.position.z = 10;
+
+function render() {
+	requestAnimationFrame( render );
+
+	if (text) {
+		text.rotation.y += 0.1;
+	}
+
+	renderer.render( scene, camera );
+}
+render();
