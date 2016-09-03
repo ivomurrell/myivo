87a58dcdf926c8b0e678a60bd7bdeaaae9b1a716
Ivo Murrell <ivoisbelongtous@gmail.com>
2016-09-03 22:07:11 +0100

    Fix the aspect ration to 16:9.
    Means that the text will fit within the screen width regardless of screen size.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera( 75, 16 / 9, 0.1, 1000 );
+camera.zoom = 0.05;
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( window.innerWidth, window.innerWidth * 9 / 16 );
 renderer.setClearColor( 0xffffff );
 document.body.appendChild( renderer.domElement );
 
