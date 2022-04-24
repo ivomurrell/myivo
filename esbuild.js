e4eb05664e59da46a3065b84b5118759726a5b74
Ivo Murrell <ivoivo@hey.com>
2022-04-24 15:53:19 +0100

    Use sass plugin to bundle sass files with esbuild

--- esbuild.js
+++ esbuild.js
@@ -1,21 +1,23 @@
 const esbuild = require('esbuild')
+const sassPlugin = require("esbuild-plugin-sass");
 
+/** @type {esbuild.BuildOptions} */
 const baseOptions = {
-  entryPoints: ['ts/main.ts'],
+  entryPoints: ['ts/main.ts', "sass/main.scss"],
   bundle: true,
-  logLevel: "info"
+  logLevel: "info",
+  outdir: 'build',
+  plugins: [sassPlugin()]
 }
 
 if (process.env.ESBUILD_PRODUCTION) {
   esbuild.build({
     ...baseOptions,
     minify: true,
-    outfile: 'build/app.min.js',
   }).catch(() => process.exit(1))
 } else {
   esbuild.build({
     ...baseOptions,
     sourcemap: true,
-    outfile: 'build/app.js',
   }).catch(() => process.exit(1))
 }
