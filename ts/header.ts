8c4d06ee29ec3349f459378e7766a6868886ec84
Ivo Murrell <ivoivo@hey.com>
2022-04-24 10:52:46 +0100

    Get build working with latest compiler version

--- ts/header.ts
+++ ts/header.ts
@@ -4,7 +4,7 @@ $(() => {
 
 		// Title must be showing before scrollWidth is calculated.
 		title.show();
-		if (title[0].scrollWidth > Math.ceil(title.innerWidth())) {
+		if (title[0].scrollWidth > Math.ceil(title.innerWidth()!)) {
 			return title.hide();
 		}
 	}
