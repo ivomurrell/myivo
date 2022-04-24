57aa16193f47cd73c4b30c6042a981e666e3927c
Ivo Murrell <ivoivo@hey.com>
2022-04-25 00:29:30 +0100

    Add autoprefixer step to CSS generation
    Need to use an alternative sass esbuild plugin to get this working: you
    can't have a postcss plugin run on the generated CSS from the sass
    plugin so you need a sass plugin that can do the postcss itself.

--- esbuild.js
+++ esbuild.js
@@ -1,6 +1,8 @@
 // @ts-check
+const autoprefixer = require("autoprefixer");
 const esbuild = require("esbuild");
-const sassPlugin = require("esbuild-plugin-sass");
+const { sassPlugin } = require("esbuild-sass-plugin");
+const postcss = require("postcss");
 
 /** @type {esbuild.BuildOptions} */
 const baseOptions = {
@@ -8,7 +10,15 @@ const baseOptions = {
   bundle: true,
   logLevel: "info",
   outdir: "build",
-  plugins: [sassPlugin()],
+  plugins: [
+    sassPlugin({
+      async transform(source) {
+        // @ts-ignore postcss type definition seems to be borked
+        const { css } = await postcss([autoprefixer]).process(source);
+        return css;
+      },
+    }),
+  ],
 };
 
 if (process.env.ESBUILD_PRODUCTION) {
