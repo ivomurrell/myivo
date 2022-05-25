34d0e33f46429a4903c9bbc8fad84153ee0497b6
Ivo Murrell <ivoivo@hey.com>
2022-05-26 00:43:24 +0100

    Avoid contention causing multiple requests to API
    If multiple tasks see that the scrobble cache is out of date
    simultaneously, they will all requests write access concurrently. If
    there is no check after they have been given the lock that another task
    hasn't already got there first then multiple tasks could make the same
    call to the last.fm API, writing and re-writing over a perfectly fresh
    value.

--- server/src/scrobble_monitor.rs
+++ server/src/scrobble_monitor.rs
@@ -29,22 +29,33 @@ impl ScrobbleMonitor {
     }
 
     pub async fn get_scrobble(&mut self) -> anyhow::Result<String> {
+        let is_fresh = |fetch_time: &Instant| fetch_time.elapsed() < Duration::from_secs(30);
+
         if let Some(scrobble) = &*self.last_scrobble.read().await {
-            if scrobble.fetch_time.elapsed() < Duration::from_secs(30) {
+            if is_fresh(&scrobble.fetch_time) {
                 tracing::debug!("returning recently fetched scrobble data");
                 return Ok(scrobble.data.clone());
             }
         }
 
-        tracing::debug!("fetching new scrobble data");
-        // try and prevent multiple handlers calling the API at the same time
-        let mut write_guard = self.last_scrobble.write().await;
-        let latest = self.fetch_scrobble().await?;
-        *write_guard = Some(Scrobble {
-            data: latest.clone(),
-            fetch_time: Instant::now(),
-        });
-        Ok(latest)
+        let mut last_scrobble = self.last_scrobble.write().await;
+        match &*last_scrobble {
+            // make sure another task hasn't fetched the new data first after we
+            // both waited for write access
+            Some(scrobble) if is_fresh(&scrobble.fetch_time) => {
+                tracing::debug!("returning (very) recently fetched scrobble data");
+                Ok(scrobble.data.clone())
+            }
+            _ => {
+                tracing::debug!("fetching new scrobble data");
+                let latest = self.fetch_scrobble().await?;
+                *last_scrobble = Some(Scrobble {
+                    data: latest.clone(),
+                    fetch_time: Instant::now(),
+                });
+                Ok(latest)
+            }
+        }
     }
 
     async fn fetch_scrobble(&self) -> anyhow::Result<String> {
